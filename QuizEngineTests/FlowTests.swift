2f0743fa5c3ee54ef6add103a09ff9e15f8c6be8
Eldorbek <eldorbek.nusratov.98@gmail.com>
2022-05-07 11:26:40 +0500

    Make `router` global variable

--- QuizEngineTests/FlowTests.swift
+++ QuizEngineTests/FlowTests.swift
@@ -10,8 +10,9 @@ import XCTest
 @testable import QuizEngine
 
 class FlowTests: XCTestCase {
+    let router = RouterSpy()
+
     func test_start_withNoQuestions_doesNotRouteToQuestion() {
-        let router = RouterSpy()
         let sut = Flow(questions: [], router: router)
 
         sut.start()
@@ -20,7 +21,6 @@ class FlowTests: XCTestCase {
     }
 
     func test_start_withOneQuestion_routesToCorrectQuestion() {
-        let router = RouterSpy()
         let sut = Flow(questions: ["Q1"], router: router)
 
         sut.start()
@@ -29,7 +29,6 @@ class FlowTests: XCTestCase {
     }
 
     func test_start_withOneQuestion_routesToCorrectQuestion_2() {
-        let router = RouterSpy()
         let sut = Flow(questions: ["Q2"], router: router)
 
         sut.start()
@@ -38,7 +37,6 @@ class FlowTests: XCTestCase {
     }
 
     func test_start_withTwoQuestion_routesToFirstQuestion() {
-        let router = RouterSpy()
         let sut = Flow(questions: ["Q1", "Q2"], router: router)
 
         sut.start()
@@ -47,7 +45,6 @@ class FlowTests: XCTestCase {
     }
 
     func test_startTwice_withTwoQuestion_routesToFirstQuestionTwice() {
-        let router = RouterSpy()
         let sut = Flow(questions: ["Q1", "Q2"], router: router)
 
         sut.start()
@@ -57,7 +54,6 @@ class FlowTests: XCTestCase {
     }
 
     func test_startAndAnswerFirstQuestion_withTwoQuestion_routesToSecondQuestion() {
-        let router = RouterSpy()
         let sut = Flow(questions: ["Q1", "Q2"], router: router)
 
         sut.start()
