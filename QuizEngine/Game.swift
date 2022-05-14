de59a45fbdd9f63148d66cd4b00b36983993e1b6
Eldorbek <eldorbek.nusratov.98@gmail.com>
2022-05-14 10:58:27 +0500

    Add private `scoring` function

--- QuizEngine/Game.swift
+++ QuizEngine/Game.swift
@@ -15,9 +15,15 @@ public class Game<Question, Answer, R: Router> where R.Question == Question, R.A
     }
 }
 
-public func startGame<Question, Answer, R: Router>(questions: [Question], router: R, answers: [Question: Answer]) -> Game<Question, Answer, R> where R.Question == Question, R.Answer == Answer {
-    let flow = Flow(questions: questions, router: router, scoring: { _ in 1 })
+public func startGame<Question, Answer: Equatable, R: Router>(questions: [Question], router: R, answers: [Question: Answer]) -> Game<Question, Answer, R> where R.Question == Question, R.Answer == Answer {
+    let flow = Flow(questions: questions, router: router, scoring: { scoring($0, correctAnswers: answers) })
     flow.start()
 
     return Game(flow: flow)
+}
+
+private func scoring<Question: Hashable, Answer: Equatable>(_ answers: [Question: Answer], correctAnswers: [Question: Answer]) -> Int {
+    return answers.reduce(0) { score, tupple in
+        return score + (correctAnswers[tupple.key] == tupple.value ? 1 : 0)
+    }
 }
--- QuizEngineTests/GameTest.swift
+++ QuizEngineTests/GameTest.swift
@@ -10,14 +10,34 @@ import XCTest
 import QuizEngine
 
 class GameTest: XCTestCase {
-    func test_startGame_answerOneOutOfTwoCorrectly_scores1() {
-        let router = RouterSpy()
-        let game = startGame(questions: ["Q1", "Q2"], router: router, answers: ["Q1": "A1", "Q2": "A2"])
+    let router = RouterSpy()
+    var game: Game<String, String, RouterSpy>!
+
+    func test_startGame_answerZeroOutOfTwoCorrectly_scoresZero() {
+        game = startGame(questions: ["Q1", "Q2"], router: router, answers: ["Q1": "A1", "Q2": "A2"])
+
+        router.answerCallback("wrong")
+        router.answerCallback("wrong")
+
+        XCTAssertEqual(router.routedResult?.score, 0)
+    }
+
+    func test_startGame_answerOneOutOfTwoCorrectly_scoresOne() {
+        game = startGame(questions: ["Q1", "Q2"], router: router, answers: ["Q1": "A1", "Q2": "A2"])
 
         router.answerCallback("A1")
         router.answerCallback("wrong")
 
         XCTAssertEqual(router.routedResult?.score, 1)
+    }
+
+    func test_startGame_answerTwoOutOfTwoCorrectly_scoresTwo() {
+        game = startGame(questions: ["Q1", "Q2"], router: router, answers: ["Q1": "A1", "Q2": "A2"])
+
+        router.answerCallback("A1")
+        router.answerCallback("A2")
+
+        XCTAssertEqual(router.routedResult?.score, 2)
     }
 }
 
