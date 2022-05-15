85b477531898499c08e85290dcb12c777b45b6fd
Eldorbek <eldorbek.nusratov.98@gmail.com>
2022-05-15 08:01:28 +0500

    Extract duplication by moving duplicated codes to `setUp` method

--- QuizEngineTests/GameTest.swift
+++ QuizEngineTests/GameTest.swift
@@ -13,9 +13,13 @@ class GameTest: XCTestCase {
     let router = RouterSpy()
     var game: Game<String, String, RouterSpy>!
 
-    func test_startGame_answerZeroOutOfTwoCorrectly_scoresZero() {
-        game = startGame(questions: ["Q1", "Q2"], router: router, answers: ["Q1": "A1", "Q2": "A2"])
+    override func setUp() {
+        super.setUp()
 
+        game = startGame(questions: ["Q1", "Q2"], router: router, answers: ["Q1": "A1", "Q2": "A2"])
+    }
+
+    func test_startGame_answerZeroOutOfTwoCorrectly_scoresZero() {
         router.answerCallback("wrong")
         router.answerCallback("wrong")
 
@@ -23,8 +27,6 @@ class GameTest: XCTestCase {
     }
 
     func test_startGame_answerOneOutOfTwoCorrectly_scoresOne() {
-        game = startGame(questions: ["Q1", "Q2"], router: router, answers: ["Q1": "A1", "Q2": "A2"])
-
         router.answerCallback("A1")
         router.answerCallback("wrong")
 
@@ -32,8 +34,6 @@ class GameTest: XCTestCase {
     }
 
     func test_startGame_answerTwoOutOfTwoCorrectly_scoresTwo() {
-        game = startGame(questions: ["Q1", "Q2"], router: router, answers: ["Q1": "A1", "Q2": "A2"])
-
         router.answerCallback("A1")
         router.answerCallback("A2")
 
