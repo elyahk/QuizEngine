36d7017051582a785fc69356c28674f676938689
Eldorbek <eldorbek.nusratov.98@gmail.com>
2022-05-15 08:32:40 +0500

    Move `Router` file to its own file

--- QuizEngine/Flow.swift
+++ QuizEngine/Flow.swift
@@ -7,20 +7,6 @@
 
 import Foundation
 
-public protocol Router {
-    associatedtype Question: Hashable
-    associatedtype Answer
-
-    typealias AnswerCallback = (Answer) -> Void
-    func routeTo(question: Question, answerCallback: @escaping AnswerCallback)
-    func routeTo(result: Result<Question, Answer>)
-}
-
-public struct Result<Question: Hashable, Answer> {
-    public let answers: [Question: Answer]
-    public let score: Int
-}
-
 class Flow <Question, Answer, R: Router> where R.Question == Question, R.Answer == Answer {
     private let questions: [Question]
     private let router: R
--- QuizEngine/Router.swift
+++ QuizEngine/Router.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+public protocol Router {
+    associatedtype Question: Hashable
+    associatedtype Answer
+
+    typealias AnswerCallback = (Answer) -> Void
+    func routeTo(question: Question, answerCallback: @escaping AnswerCallback)
+    func routeTo(result: Result<Question, Answer>)
+}
+
+public struct Result<Question: Hashable, Answer> {
+    public let answers: [Question: Answer]
+    public let score: Int
+}
