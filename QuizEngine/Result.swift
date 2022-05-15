e24ef56119648be530c26d67a523a95f0da5c132
Eldorbek <eldorbek.nusratov.98@gmail.com>
2022-05-15 11:11:08 +0500

    Route to result shows result view controller

--- QuizEngine/Result.swift
+++ QuizEngine/Result.swift
@@ -8,4 +8,9 @@
 public struct Result<Question: Hashable, Answer> {
     public let answers: [Question: Answer]
     public let score: Int
+
+    public init(answers: [Question: Answer], score: Int) {
+        self.answers = answers
+        self.score = score
+    }
 }
